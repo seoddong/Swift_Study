6eee3b6040be695cda83462fdc7a4d013378a8b1
SeoDongHee <seoddong@hanmail.net>
2016-04-17 22:19:10 +0900

    Initial Commit

---
+++ SampleControllView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleControllView
+//
+//  Created by SeoDongHee on 2016. 4. 17..
+//  Copyright © 2016년 SeoDongHee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
