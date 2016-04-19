22d3ad8cd63f930dfaab05ce2c49e6c60c4b9d97
seoddong <jolajoayo@gmail.com>
2016-04-20 00:04:37 +0900

    Added files via upload

---
+++ SampleNavigationController/SampleNavigationController/YellowVC.swift
@@ -0,0 +1,38 @@
+//
+//  YellowVC.swift
+//  SampleNavigationController
+//
+//  Created by SeoDongHee on 2016. 4. 19..
+//  Copyright © 2016년 SeoDongHee. All rights reserved.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToRed(sender: AnyObject) {
+        navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
